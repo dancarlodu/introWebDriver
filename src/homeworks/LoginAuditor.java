3aefca7df75515154e06bef79cc57f6c19bb5584
HÉCTOR DANIEL CARDONA <hectordc@INGIMPSOFLDR.audi.d1>
2020-08-12 20:18:49 -0500

    Validación de ingreso al Auditor

--- src/homeworks/LoginAuditor.java
+++ src/homeworks/LoginAuditor.java
@@ -12,40 +12,37 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.Select;
 
 public class LoginAuditor {
 	
 	static WebDriver driver;
-	static WebDriverWait wait;
+	static WebDriverWait myWaitVar;
 	
 
 	public static void main(String[] args) throws InterruptedException  {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub	
 		
 		
-			
-		
-			for (int i = 0; i < 3 ; i++) {
-			
 			setUp ("firefox","http://www.auditor.net.co/auditor/");
+			
+			for (int i = 0; i < 5 ; i++) {
 		
-			loginAuditor("hectordc","12aaju","PEREIRA ESPECIALIZADO MAC");
-			
-			validacion();
-			
-			salirAuditor();		
+				loginAuditor("hectordc","12aaju","CIUDAD PEREIRA");
 				
-			System.out.println("Caso de prueba exitoso: "+i);
+				validacion();
+				
+				salirAuditor();
+				
+				System.out.println("Caso de prueba exitoso: "+i);			
+				
+			}
 			
 			closeBrowser();
-		
-			
-		}
 			
 	
 		
 	}
+	
 	
 	private static void setUp (String browser, String url) {
 		switch(browser) {
@@ -65,6 +62,7 @@ public class LoginAuditor {
 		}
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(20,  TimeUnit.SECONDS); 
+		driver.manage().deleteAllCookies();
 		driver.get(url);
 		
 		
@@ -75,25 +73,21 @@ public class LoginAuditor {
 		private static void salirAuditor() {
 		// TODO Auto-generated method stub
 			
-			WebDriverWait wait = new WebDriverWait(driver, 30);
-			wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(0));
-			
-			//driver.switchTo().frame(0);
-			WebElement validacionIngreso = driver.findElement(By.xpath("//*[@id=\"pagSuperior:lblUsuario\"]"));
-			validacionIngreso.getText();			
 			
 			
-			if(validacionIngreso.equals("HECTOR DANIEL CARDONA - PEREIRA ESPECIALIZADO MAC (PEREIRA)"))
-					 
-				System.out.println("Error ingreso ! ");
-					
-			else 
-				System.out.println("Se ingreso correctamente");
+			driver.switchTo().frame(1);
+			WebElement validacionIngreso = driver.findElement(By.xpath("//*[contains(text(),'HECTORDC')]"));
 			
-			// Click en el botón salir
-			//Thread.sleep(15000);			
-			//driver.switchTo().frame("topFrame");
-			//driver.switchTo().frame(0);
+			if (validacionIngreso.isDisplayed())
+				System.out.println("Usuario registrado en Auditor");
+			else
+				System.exit(-1);			
+			
+			
+			//Click en el botón salir
+			//Thread.sleep(15000);
+			driver.switchTo().parentFrame();
+			driver.switchTo().frame(0); // topFrame
 			WebElement botonSalir = driver.findElement(By.xpath("//input[@id='pagSuperior:btnSalir2']"));
 			botonSalir.click();
 			
@@ -103,7 +97,9 @@ public class LoginAuditor {
 			Alert alert = driver.switchTo().alert();
 			//String alertaSalir = alert.getText();
 			//System.out.println(alertaSalir);
-			alert.accept();				
+			alert.accept();
+			
+			System.out.println("Saliendo del CAF. Vuelva pronto ..."); 
 			
 			
 		
@@ -112,9 +108,15 @@ public class LoginAuditor {
 		private static void loginAuditor(String usuario, String password, String caf)  {
 		// TODO Auto-generated method stub
 			
+						
+			//WebDriverWait myWaitVar = new WebDriverWait(driver, 30);
 			
+			driver.switchTo().defaultContent();
 			
+			boolean advertenciaPresente;
+			int contadorIntentosIngreso=1;
 			
+			do {			
 			// Ingreso del usuario
 			WebElement campoUsuario = driver.findElement(By.id("pagLogin:txtUsuario"));
 			campoUsuario.clear();
@@ -132,119 +134,54 @@ public class LoginAuditor {
 			botonSeleccionCaf.click();
 			WebElement campoCaf = driver.findElement(By.name("pagLogin:txtCafcomboboxField"));
 			campoCaf.clear();
-			campoCaf.sendKeys(caf);
+			campoCaf.sendKeys(caf);	
 			
-			//Ingresar del CAF
-			WebElement botonIngresar = driver.findElement(By.id("pagLogin:btnIngresar")); 
-			botonIngresar.click(); 
-			
-			
-			
-			
-			
-			
-		
-			
-			
-			
-		
 			//WebElement seleccionCaf = driver.findElement(By.cssSelector(".rich-combobox-item-selected"));
 			//seleccionCaf.click();
 			//myWaitVar.until(ExpectedConditions.elementToBeSelected(By.cssSelector(".rich-combobox-item-selected"))).click();
-			
-			
-			
-			
-			//driver.findElement(By.xpath("//input[@id='pagLogin:txtCafcomboboxButton']")).click();
-			//myWaitVar.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='pagLogin:txtCafcomboboxButton']"))).click();
-			//myWaitVar.until(ExpectedConditions.visibilityOfElementLocated (By.id("pagLogin:txtCafcomboboxField"))).sendKeys(caf);
-			//myWaitVar.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".rich-combobox-item-selected"))).click();
-			
-		
-			
-							
-		
-			
-				/*
-				 * WebElement advertenciaIngreso =
-				 * driver.findElement(By.xpath("//*[@id=\"pagLogin:error\"]"));
-				 * if(advertenciaIngreso.
-				 * equals("El caf ingresado no está permitido para éste usuario")) {
-				 * 
-				 * System.out.println("Error selección CAF ! ");
-				 * 
-				 * 
-				 * } else {
-				 * 
-				 * System.out.println("El caf se ingreso correctamente"); WebElement
-				 * botonIngresar = driver.findElement(By.id("pagLogin:btnIngresar"));
-				 * botonIngresar.click();
-				 * 
-				 * }
-				 */
-			
-			
-			//driver.findElement(By.id("pagLogin:txtCafcomboboxField")).sendKeys(caf);
-			//driver.findElement(By.xpath("//input[@id='pagLogin:txtCafcomboboxButton']")).click();
-			
-			//myWaitVar.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".rich-combobox-item-selected"))).click();
-			//driver.findElement(By.id("pagLogin:txtCafcomboboxField")).click();
-			//driver.findElement(By.id("pagLogin:txtCafcomboboxButton")).click();
-			//driver.findElement(By.id("pagLogin:txtCafcomboboxField")).clear();
-			
-			//myWaitVar.until(ExpectedConditions.visibilityOfElementLocated(By.id("pagLogin:txtCafcomboboxField")));
-		
-			//driver.findElement(By.cssSelector(".rich-combobox-item-selected")).click();
-			
-			
-			
-			
-			/*
-			 * WebDriverWait wait = new WebDriverWait(driver, 15);
-			 * 
-			 * WebElement listaCaf = (WebElement) wait.until
-			 * (ExpectedConditions.numberOfElementsToBeMoreThan(
-			 * By.cssSelector("span.rich-combobox-item.rich-combobox-item-selected"), 1));
-			 * 
-			 * 
-			 * 
-			 * 
-			 * listaCaf.click();
-			 */
-			 
-			//driver.findElement(By.cssSelector("span.rich-combobox-item.rich-combobox-item-selected")).click();
-			
-			//driver.findElement(By.id("pagLogin:txtCafcomboboxValue")).clear();
-			//driver.findElement(By.id("pagLogin:txtCafcomboboxValue")).sendKeys(caf);
-			 
-			 
-			
-			//WebElement listaCaf = driver.findElement(By.id("pagLogin:txtCafcomboboxField"));
-			//listaCaf.clear();
-			//listaCaf.sendKeys(caf);			
-			
-				//WebDriverWait myWaitVar = new WebDriverWait(driver, 20);
-				//myWaitVar.until(ExpectedConditions.elementToBeClickable (By.id("pagLogin:btnIngresar")));
-				//WebElement botonIngresar = driver.findElement(By.id("pagLogin:btnIngresar"));
-				//Thread.sleep(5000);
-				//botonIngresar.click();
+	
 				
-
-			//pagLogin:txtCafcomboboxButton
-			// pagLogin:txtCafcomboBoxButtonBG
-			// pagLogin:txtCafcomboboxValue
+			
+			//Ingresar del CAF
+			WebElement botonIngresar = driver.findElement(By.id("pagLogin:btnIngresar")); 
+			botonIngresar.click();
+			
+			
+			
+			//Validación de ingreso			
+			advertenciaPresente = driver.findElements(By.xpath("//*[contains (@title, 'Advertencia:')]")).size()  > 0;		
+			
+			
+			if (advertenciaPresente) {				
+			
+				WebElement mensajeAdvertencia = driver.findElement(By.xpath("//*[contains (@title, 'Advertencia:')]"));					
 		
+					System.out.println(mensajeAdvertencia.getText());
+					System.out.println("NO ingreso al CAF !");
+					System.exit(-1);
+					closeBrowser();
+					
+				} else {
+					System.out.println("Ingresando al CAF ..."); 
+					
+					}
+			
+			System.out.println("Intento de ingreso: "+contadorIntentosIngreso);
+			contadorIntentosIngreso=contadorIntentosIngreso+1;
+			}			
+			while (advertenciaPresente==true);
+	
 	}
 		
 		private static void validacion() {
 		// TODO Auto-generated method stub
-		
-	}
-		
+			
+	
+		}
 		
 		private static void closeBrowser() {		
 			
-			//driver.close();
+			//driver.close();			
 			driver.quit();
 			
 			
